25d45a77259aa381439b9ae39325fffb0cdae17f
Jzhou271 <zhou.jing4@northeastern.edu>
2023-02-23 08:16:55 -0800

    files update

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
 import React from 'react';
-import HouseScreen from './screens/house-screen.js';
-import AddScreen from './screens/add-screen.js';
-import TodoScreen from './screens/todo-screen.js';
-import DestructorScreen from './screens/destructor-screen.js';
-import NavBar from './components/nav-bar.js';
-import Tarp from './tarp/index.js';
+import Labs from "./labs";
+import Tuiter from "./tuiter";
+import HelloWorld from "./labs/a6/hello-world";
 import {BrowserRouter} from 'react-router-dom';
-import {Routes,Route} from 'react-router';
+import {Routes, Route} from 'react-router';
 
 function App() {
   return(
-      <div className="container-fluid mt-4">
-        <BrowserRouter>
-          <div className="row">
-            <div className="col-1">
-              <NavBar/>
-            </div>
-            <div className="col-11">
-              <Routes>
-                <Route path="/tarp/*" element={<Tarp/>}/> // Use uppercase "T" for the component
-                <Route path="/destructors" element={<DestructorScreen/>}/>
-                <Route path="/add/:paramA/:paramB" element={<AddScreen/>}/>
-                <Route path="/house" element={<HouseScreen/>}/>
-                <Route path="/todos" element={<TodoScreen/>}/>
-              </Routes>
-            </div>
-          </div>
-        </BrowserRouter>
-      </div>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route index element={<Labs/>}/>
+            <Route path="/hello" element={<HelloWorld/>}/>
+            <Route path="/tuiter" element={<Tuiter/>}/>
+          </Routes>
+        </div>
+      </BrowserRouter>
   );
 }
 export default App;
